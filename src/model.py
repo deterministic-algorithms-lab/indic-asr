8ce446adc9b7524900370436f043f19fca0c9a09
Jaskaran Singh <jaskaransingh101010@gmail.com>
2021-02-19 17:20:08 +0530

    training script added

---
+++ src/model.py
@@ -0,0 +1,30 @@
+import torch
+import torch.nn as nn
+
+from transformers import Wav2Vec2Model, Wav2Vec2ForCTC
+from configs import  config
+# class Model:
+    
+#     def __init__(self,):
+#         model=
+
+def get_model(tokenizer):
+    
+    model = Wav2Vec2ForCTC.from_pretrained(config.model)
+    pt_wts = model.lm_head.weight
+    pt_bias = model.lm_head.bias
+
+    new_lm_head = nn.Linear(pt_wts.shape[1], len(tokenizer))
+
+    init_wts = new_lm_head.weight.clone().detach()
+    init_bs = new_lm_head.bias.clone().detach()
+    init_wts[:pt_wts.shape[0], :] = pt_wts.clone().detach()
+    init_bs[:pt_bias.shape[0]] = pt_bias.clone().detach()
+
+    with torch.no_grad():
+        new_lm_head.weight = nn.Parameter(init_wts)
+        new_lm_head.bias = nn.Parameter(init_bs)
+
+    model.lm_head = new_lm_head
+    
+    return model
---
+++ src/tokenizer.py
@@ -0,0 +1,50 @@
+import torch
+import torch.nn as nn
+from typing import List, Tuple
+from transformers import Wav2Vec2Tokenizer
+
+class Wav2Vec2Tok(Wav2Vec2Tokenizer):
+    """
+    Extending the base tokenizer of Wav2Vec2 for the purpose of encoding
+    text sequences. 
+    """
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for i in range(2304, 2432) :
+            self._add_tokens(chr(i))
+             
+        
+    def tokenize(self, text: str, **kwargs) -> List[int]:
+        """
+        Converts a single str into a sequence of token ids.
+        """
+        text = ' '.join(text.split(' '))
+        text = text.replace(' ', self.word_delimiter_token)
+        tokens = [self.bos_token_id]
+        
+        for char in text:
+            tokens.append(self._convert_token_to_id(char))
+
+        tokens.append(self.eos_token_id)
+        return tokens
+    
+    def pad_batch_sentences(self, sentences: List[List[int]], max_length: int=-1) -> Tuple[torch.FloatTensor, torch.IntTensor]:
+        """
+        Pads all list of token ids, in a batch to the maximum length.
+        Truncates all sequences to max_length.
+        """
+        sentences = [sentence[:max_length] for sentence in sentences]
+        lengths = [len(sentence) for sentence in sentences]
+        max_len = max(lengths)
+        for i, sentence in enumerate(sentences):
+            sentences[i] = sentence + [self.pad_token_id]*(max_len-len(sentence))
+        return torch.tensor(sentences, dtype=torch.float32), torch.tensor(lengths)
+    
+    def batch_tokenize(self, texts: List[str], **kwargs) -> Tuple[torch.FloatTensor, torch.IntTensor]:
+        """
+        Tokenizes and batches together a list of texts
+        """
+        tokenized_sentences = []
+        for sentence in texts:
+            tokenized_sentences.append(self.tokenize(sentence))
+        return self.pad_batch_sentences(tokenized_sentences)
