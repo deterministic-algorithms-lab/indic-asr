51c86832623d55d1f2cf5ea01c4a5245508c47f4
Jaskaran Singh <jaskaransingh101010@gmail.com>
2021-03-24 17:07:15 +0530

    mutli lang monolingual support added

--- src/model.py
+++ src/model.py
@@ -57,17 +57,17 @@ def get_model(tokenizer):
     pt_bias = model.lm_head.bias
 
     new_lm_head = nn.Linear(pt_wts.shape[1], len(tokenizer))
+    if config.transliterate:
+        init_wts = new_lm_head.weight.clone().detach()
+        init_bs = new_lm_head.bias.clone().detach()
+        init_wts[:pt_wts.shape[0], :] = pt_wts.clone().detach()
+        init_wts[pt_wts.shape[0]:, :] = torch.mean(pt_wts.clone().detach(), dim=0)
+        init_bs[:pt_bias.shape[0]] = pt_bias.clone().detach()
+        init_bs[pt_wts.shape[0]:] = torch.mean(pt_bias.clone().detach(), dim=0)
 
-    init_wts = new_lm_head.weight.clone().detach()
-    init_bs = new_lm_head.bias.clone().detach()
-    init_wts[:pt_wts.shape[0], :] = pt_wts.clone().detach()
-    init_wts[pt_wts.shape[0]:, :] = torch.mean(pt_wts.clone().detach(), dim=0)
-    init_bs[:pt_bias.shape[0]] = pt_bias.clone().detach()
-    init_bs[pt_wts.shape[0]:] = torch.mean(pt_bias.clone().detach(), dim=0)
-
-    with torch.no_grad():
-        new_lm_head.weight = nn.Parameter(init_wts)
-        new_lm_head.bias = nn.Parameter(init_bs)
+        with torch.no_grad():
+            new_lm_head.weight = nn.Parameter(init_wts)
+            new_lm_head.bias = nn.Parameter(init_bs)
 
     model.lm_head = new_lm_head
     
